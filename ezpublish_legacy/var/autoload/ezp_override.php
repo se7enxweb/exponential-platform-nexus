8a18f0060f61f0aecca16aed98d5234b333e1d27
7x <info@se7enx.com>
2026-02-15 02:08:07 -0800

    Added: Added based var dir (including the) site storage dir (images in site content within the db content tree) to the base repository to ensure default installations function as designed. Bugfix.

---
+++ ezpublish_legacy/var/autoload/ezp_override.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Autoloader definition for eZ Publish Kernel overrides files.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ * @package kernel
+ *
+ */
+
+return array(
+      'eZContentFunctionCollection'   => 'extension/nxc_powercontent/modules/content/ezcontentfunctioncollection.php',
+      'eZContentOperationCollection'  => 'extension/nxc_powercontent/modules/content/ezcontentoperationcollection.php',
+      'ezpContentPublishingBehaviour' => 'extension/nxc_powercontent/modules/content/ezcontentpublishingbehaviour.php',
+    );
+
+?>
